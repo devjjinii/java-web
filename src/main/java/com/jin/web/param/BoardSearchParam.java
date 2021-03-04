b1dc7f3954c5578ac2e6a65f0a7acc7d2f835af8
devjjinii <devjjinii@naver.com>
2021-03-04 15:37:13 +0900

    feat: 배열 검색 조건 추가

--- src/main/java/com/jin/web/param/BoardSearchParam.java
+++ src/main/java/com/jin/web/param/BoardSearchParam.java
@@ -1,7 +1,10 @@
 package com.jin.web.param;
 
+import com.jin.web.dto.BoardType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * BoardSearch 추가 이유
@@ -12,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class BoardSearchParam {
 
     private String searchKeyword; // 검색조건
+    private List<BoardType> boardTypes; // 게시판 종류 배열
 
     public BoardSearchParam() {
     }
